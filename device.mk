9076ddd90fe625d405db4704b8a39fe68eac196e
Jason <leejiangyee971027@gmail.com>
2026-03-11 17:09:31 +0800

    cepheus: add settingslibdevice RROs

--- device.mk
+++ device.mk
@@ -37,6 +37,7 @@ PRODUCT_PACKAGES += \
     FrameworkResOverlayDevice \
     LineageSDKOverlayDevice \
     LineageSystemUIOverlayDevice \
+    SettingsLibOverlayDevice \
     SettingsOverlayDevice \
     SystemUIOverlayDevice
 
