48cde33c962bb050dbbb334ef99669d8a9f0f9a6
JoKeRLeE <leejiangyee971027@gmail.com>
2026-03-23 03:08:29 +0800

    cepheus: import overlays from official source
    https://github.com/Evolution-X-Devices/device_xiaomi_cepheus/tree/bka

--- device.mk
+++ device.mk
@@ -49,12 +49,15 @@ $(call soong_config_set,xiaomi_msmnile,variant_lib,//$(LOCAL_PATH):libvariant_xi
 
 # Overlays
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
     FrameworkResOverlayDevice \
     LineageSDKOverlayDevice \
     LineageSystemUIOverlayDevice \
     SettingsLibOverlayDevice \
     SettingsOverlayDevice \
-    SystemUIOverlayDevice
+    SystemUIOverlayDevice \
+    TelephonyOverlay \
+    WifiOverlay
 
 # Power
 PRODUCT_COPY_FILES += \
