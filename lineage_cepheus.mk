2d579fc92853dd5fbdee831487d9740b3ac27d19
JoKeRLeE <leejiangyee971027@gmail.com>
2026-09-01 14:04:15 +0000

    cepheus: initial bring up for evolution x (unofficial)

--- lineage_cepheus.mk
+++ lineage_cepheus.mk
@@ -8,29 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Infinity stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from cepheus device
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
-# Infinity Stuff
+EVO_BUILD_TYPE := Unofficial
+PERF_ANIM_OVERRIDE := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BUILD_APERTURE_CAMERA := false
 TARGET_HAS_UDFPS := true
-WITH_GMS := true
-WITH_GAPPS := true
-INFINITY_MAINTAINER := "JoKeRLeE"
+TARGET_INCLUDE_ACCORD := false
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := infinity_cepheus
+PRODUCT_NAME := lineage_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="cepheus-user 11 RKQ1.200826.002 V12.5.1.0.RFAMIXM release-keys" \
-    BuildFingerprint=Xiaomi/cepheus/cepheus:11/RKQ1.200826.002/V12.5.1.0.RFAMIXM:user/release-keys
