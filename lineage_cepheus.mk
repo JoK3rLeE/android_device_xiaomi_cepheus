01ce8c3ac6df4375220e1cf7f1609279439e7bcf
JoKeRLeE <leejiangyee971027@gmail.com>
2026-09-21 13:50:22 +0000

    cepheus: adapt lineage build to debug A17

--- lineage_cepheus.mk
+++ lineage_cepheus.mk
@@ -8,17 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from cepheus device
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
-EVO_BUILD_TYPE := Unofficial
-PERF_ANIM_OVERRIDE := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BUILD_APERTURE_CAMERA := false
+# Specific Flags
 TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_APERTURE_CAMERA := true
 TARGET_INCLUDE_ACCORD := false
 
 # Device identifier. This must come after all inclusions.
